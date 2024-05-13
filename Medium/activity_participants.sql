d6b195fd2f8e33a66169f8f3b7ad088a4ee7dd92
mithun-sudo <mithun.murugan23@gmail.com>
2024-05-13 21:46:24 +0530

    activity participants

---
+++ Medium/activity_participants.sql
@@ -0,0 +1,113 @@
+-- Table: Friends
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | name          | varchar |
+-- | activity      | varchar |
+-- +---------------+---------+
+-- id is the id of the friend and primary key for this table.
+-- name is the name of the friend.
+-- activity is the name of the activity which the friend takes part in.
+-- Table: Activities
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | name          | varchar |
+-- +---------------+---------+
+-- id is the primary key for this table.
+-- name is the name of the activity.
+ 
+
+-- Write an SQL query to find the names of all the activities with neither maximum, nor minimum number of participants.
+
+-- Return the result table in any order. Each activity in table Activities is performed by any person in the table Friends.
+
+-- The query result format is in the following example:
+
+-- Friends table:
+-- +------+--------------+---------------+
+-- | id   | name         | activity      |
+-- +------+--------------+---------------+
+-- | 1    | Jonathan D.  | Eating        |
+-- | 2    | Jade W.      | Singing       |
+-- | 3    | Victor J.    | Singing       |
+-- | 4    | Elvis Q.     | Eating        |
+-- | 5    | Daniel A.    | Eating        |
+-- | 6    | Bob B.       | Horse Riding  |
+-- +------+--------------+---------------+
+
+-- Activities table:
+-- +------+--------------+
+-- | id   | name         |
+-- +------+--------------+
+-- | 1    | Eating       |
+-- | 2    | Singing      |
+-- | 3    | Horse Riding |
+-- +------+--------------+
+
+-- Result table:
+-- +--------------+
+-- | activity     |
+-- +--------------+
+-- | Singing      |
+-- +--------------+
+
+-- Eating activity is performed by 3 friends, maximum number of participants, (Jonathan D. , Elvis Q. and Daniel A.)
+-- Horse Riding activity is performed by 1 friend, minimum number of participants, (Bob B.)
+-- Singing is performed by 2 friends (Victor J. and Jade W.)
+
+-- Solution
+
+-- Table definition for Friends
+CREATE TABLE Friends (
+    id INT PRIMARY KEY,
+    name VARCHAR,
+    activity VARCHAR
+);
+
+-- Insert statements for Friends
+INSERT INTO Friends (id, name, activity) VALUES
+(1, 'Jonathan D.', 'Eating'),
+(2, 'Jade W.', 'Singing'),
+(3, 'Victor J.', 'Singing'),
+(4, 'Elvis Q.', 'Eating'),
+(5, 'Daniel A.', 'Eating'),
+(6, 'Bob B.', 'Horse Riding');
+
+-- Table definition for Activities
+CREATE TABLE Activities (
+    id INT PRIMARY KEY,
+    name VARCHAR
+);
+
+-- Insert statements for Activities
+INSERT INTO Activities (id, name) VALUES
+(1, 'Eating'),
+(2, 'Singing'),
+(3, 'Horse Riding');
+
+
+select 
+name
+from
+activities 
+where 
+name not in 
+(select
+activity
+from
+(select
+*,
+number_of_participants = max(number_of_participants) over (order by number_of_participants desc) as flag_1,
+number_of_participants = min(number_of_participants) over (order by number_of_participants asc) as flag_2
+from
+(select 
+activity,
+count(id) as number_of_participants
+from friends
+group by activity) A) B
+where flag_1 is true or flag_2 is true)
