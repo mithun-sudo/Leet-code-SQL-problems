03e67ddfade08634bde60b205dea5cf7b726fd85
mithun-sudo <mithun.murugan23@gmail.com>
2024-05-15 16:02:52 +0530

    5 problems

---
+++ Medium/highest_answer_rate.sql
@@ -0,0 +1,62 @@
+-- Get the highest answer rate question from a table survey_log with these columns: id, action, question_id, answer_id, q_num, timestamp.
+
+-- id means user id; action has these kind of values: "show", "answer", "skip"; answer_id is not null when action column is "answer", 
+-- while is null for "show" and "skip"; q_num is the numeral order of the question in current session.
+
+-- Write a sql query to identify the question which has the highest answer rate.
+
+-- Example:
+
+-- Input:
+-- +------+-----------+--------------+------------+-----------+------------+
+-- | id   | action    | question_id  | answer_id  | q_num     | timestamp  |
+-- +------+-----------+--------------+------------+-----------+------------+
+-- | 5    | show      | 285          | null       | 1         | 123        |
+-- | 5    | answer    | 285          | 124124     | 1         | 124        |
+-- | 5    | show      | 369          | null       | 2         | 125        |
+-- | 5    | skip      | 369          | null       | 2         | 126        |
+-- +------+-----------+--------------+------------+-----------+------------+
+-- Output:
+-- +-------------+
+-- | survey_log  |
+-- +-------------+
+-- |    285      |
+-- +-------------+
+-- Explanation:
+-- question 285 has answer rate 1/1, while question 369 has 0/1 answer rate, so output 285.
+ 
+
+-- Note: The highest answer rate meaning is: answer number's ratio in show number in the same question.
+
+-- Solution
+
+
+CREATE TABLE survey_log (
+    id INT,
+    action VARCHAR(10),
+    question_id INT,
+    answer_id INT,
+    q_num INT,
+    timestamp INT
+);
+
+
+INSERT INTO survey_log (id, action, question_id, answer_id, q_num, timestamp) VALUES (5, 'show', 285, NULL, 1, 123);
+INSERT INTO survey_log (id, action, question_id, answer_id, q_num, timestamp) VALUES (5, 'answer', 285, 124124, 1, 124);
+INSERT INTO survey_log (id, action, question_id, answer_id, q_num, timestamp) VALUES (5, 'show', 369, NULL, 2, 125);
+INSERT INTO survey_log (id, action, question_id, answer_id, q_num, timestamp) VALUES (5, 'skip', 369, NULL, 2, 126);
+
+
+select
+question_id
+from
+(select
+*,
+answer_rate = max(answer_rate) over (order by answer_rate desc) as flag
+from
+(select
+question_id,
+sum(case when action = 'answer' then 1 else 0 end) * 1.0 / sum(case when action = 'show' then 1 else 0 end) as answer_rate
+from survey_log
+group by question_id) A) B
+where flag is true
