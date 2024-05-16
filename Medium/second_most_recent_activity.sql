f855d8d4b693f7a69ff04b9acf217e5d58618587
mithun-sudo <mithun.murugan23@gmail.com>
2024-05-16 14:47:05 +0530

    4 problems

---
+++ Medium/second_most_recent_activity.sql
@@ -0,0 +1,75 @@
+-- Table: UserActivity
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | username      | varchar |
+-- | activity      | varchar |
+-- | startDate     | Date    |
+-- | endDate       | Date    |
+-- +---------------+---------+
+-- This table does not contain primary key.
+-- This table contain information about the activity performed of each user in a period of time.
+-- A person with username performed a activity from startDate to endDate.
+
+-- Write an SQL query to show the second most recent activity of each user.
+
+-- If the user only has one activity, return that one. 
+
+-- A user can't perform more than one activity at the same time. Return the result table in any order.
+
+-- The query result format is in the following example:
+
+-- UserActivity table:
+-- +------------+--------------+-------------+-------------+
+-- | username   | activity     | startDate   | endDate     |
+-- +------------+--------------+-------------+-------------+
+-- | Alice      | Travel       | 2020-02-12  | 2020-02-20  |
+-- | Alice      | Dancing      | 2020-02-21  | 2020-02-23  |
+-- | Alice      | Travel       | 2020-02-24  | 2020-02-28  |
+-- | Bob        | Travel       | 2020-02-11  | 2020-02-18  |
+-- +------------+--------------+-------------+-------------+
+
+-- Result table:
+-- +------------+--------------+-------------+-------------+
+-- | username   | activity     | startDate   | endDate     |
+-- +------------+--------------+-------------+-------------+
+-- | Alice      | Dancing      | 2020-02-21  | 2020-02-23  |
+-- | Bob        | Travel       | 2020-02-11  | 2020-02-18  |
+-- +------------+--------------+-------------+-------------+
+
+-- The most recent activity of Alice is Travel from 2020-02-24 to 2020-02-28, before that she was dancing from 2020-02-21 to 2020-02-23.
+-- Bob only has one record, we just take that one.
+
+-- Solution
+
+
+CREATE TABLE UserActivity (
+    username VARCHAR(255),
+    activity VARCHAR(255),
+    startDate DATE,
+    endDate DATE
+);
+
+
+INSERT INTO UserActivity (username, activity, startDate, endDate)
+VALUES 
+('Alice', 'Travel', '2020-02-12', '2020-02-20'),
+('Alice', 'Dancing', '2020-02-21', '2020-02-23'),
+('Alice', 'Travel', '2020-02-24', '2020-02-28'),
+('Bob', 'Travel', '2020-02-11', '2020-02-18');
+
+
+select
+username,
+activity,
+startDate,
+endDate
+from
+(select
+*,
+(select count(distinct activity) from UserActivity t2 where t2.username = t1.username) as distinctActvities,
+rank() over (partition by username order by enddate) as activities_rank
+from
+UserActivity t1) A
+where (distinctActvities > 1 and activities_rank = 2) or (distinctActvities = 1)
