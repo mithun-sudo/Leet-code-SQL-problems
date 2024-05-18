87856c5429b43a0a0acb41d122f2ae8cc2e14954
mithun-sudo <mithun.murugan23@gmail.com>
2024-05-18 23:02:23 +0530

    3 problems

---
+++ Medium/product_price_at_given_date.sql
@@ -0,0 +1,69 @@
+-- Table: Products
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | product_id    | int     |
+-- | new_price     | int     |
+-- | change_date   | date    |
+-- +---------------+---------+
+-- (product_id, change_date) is the primary key of this table.
+-- Each row of this table indicates that the price of some product was changed to a new price at some date.
+ 
+
+-- Write an SQL query to find the prices of all products on 2019-08-16. Assume the price of all products before any change is 10.
+
+-- The query result format is in the following example:
+
+-- Products table:
+-- +------------+-----------+-------------+
+-- | product_id | new_price | change_date |
+-- +------------+-----------+-------------+
+-- | 1          | 20        | 2019-08-14  |
+-- | 2          | 50        | 2019-08-14  |
+-- | 1          | 30        | 2019-08-15  |
+-- | 1          | 35        | 2019-08-16  |
+-- | 2          | 65        | 2019-08-17  |
+-- | 3          | 20        | 2019-08-18  |
+-- +------------+-----------+-------------+
+
+-- Result table:
+-- +------------+-------+
+-- | product_id | price |
+-- +------------+-------+
+-- | 2          | 50    |
+-- | 1          | 35    |
+-- | 3          | 10    |
+-- +------------+-------+
+
+-- Solution
+
+CREATE TABLE Products (
+    product_id INT,
+    new_price INT,
+    change_date DATE,
+    PRIMARY KEY (product_id, change_date)
+);
+
+INSERT INTO Products (product_id, new_price, change_date) VALUES 
+(1, 20, '2019-08-14'),
+(2, 50, '2019-08-14'),
+(1, 30, '2019-08-15'),
+(1, 35, '2019-08-16'),
+(2, 65, '2019-08-17'),
+(3, 20, '2019-08-18');
+
+select
+distinct
+products.product_id,
+coalesce(B.new_price, 10) as price
+from products
+left join (select
+*
+from
+(select
+*,
+rank() over (partition by product_id order by change_date desc) as rn
+from products
+where change_date <= '2019-08-16') A
+where rn = 1) B on B.product_id = products.product_id
